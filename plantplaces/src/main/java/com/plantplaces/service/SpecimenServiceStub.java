7a8bfe2aad79b7b4edd62446c643ba5139fbb1c1
chand <chand@DESKTOP-LBR7BG1>
2019-12-30 20:11:43 -0700

    Added plantDTO

--- plantplaces/src/main/java/com/plantplaces/service/SpecimenServiceStub.java
+++ plantplaces/src/main/java/com/plantplaces/service/SpecimenServiceStub.java
@@ -1,7 +1,10 @@
 package com.plantplaces.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
+import com.plantplaces.dto.PlantDTO;
 import com.plantplaces.dto.SpecimenDTO;
 
 @Component
@@ -20,6 +23,18 @@ public class SpecimenServiceStub implements ISpecimenService {
 	@Override
 	public void save(SpecimenDTO specimenDTO) {
 		
+	}
+
+	@Override
+	public List<PlantDTO> fetchPlants(String searchTerm) {
+		// TODO Auto-generated method stub
+		if (searchTerm.contains("edbud") || searchTerm.contains("Cersis")) {
+			PlantDTO plant = new PlantDTO();
+			plant.setGenus("Cercis");
+			plant.setSpecies("canadensis");
+			plant.setCommon("Eastern Redbud");
+		}
+		return null;
 	}
 
 }
