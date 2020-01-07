1d38fec2f931a0b1e35432ea93ea9f86ac81ae03
chand <chand@DESKTOP-LBR7BG1>
2020-01-07 14:58:27 -0700

    Autocomplete feature final touch

--- plantplaces/src/main/java/com/plantplaces/PlantPlacesController.java
+++ plantplaces/src/main/java/com/plantplaces/PlantPlacesController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.plantplaces.dto.PlantDTO;
@@ -27,6 +28,10 @@ public class PlantPlacesController {
 	
 	@Autowired
 	private ISpecimenService specimenService;
+
+	private List<PlantDTO> allPlants;
+
+	private String firstThreeCharacters;
 	
 	@RequestMapping(value="/savespecimen")
 	public String saveSpecimen(SpecimenDTO specimenDTO) {
@@ -132,6 +137,31 @@ public class PlantPlacesController {
 			modelAndView.setViewName("error");
 		}
 		return modelAndView;
+	}
+	
+	@RequestMapping(value="/plantNamesAutocomplete")
+	@ResponseBody
+	public List<String> plantNamesAutocomplete(@RequestParam(value = "term", required = false, defaultValue = "") String term){
+		List<String> suggestions = new ArrayList<String>();
+		try {
+			// only update when term is three characters.
+			if (term.length() == 3) {
+			firstThreeCharacters = term;
+			allPlants = specimenService.fetchPlants(term);
+			}
+			
+			for(PlantDTO plantDTO: allPlants) {
+				if (plantDTO.toString().contains(term)) {
+				suggestions.add(plantDTO.toString());
+			   }
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			log.error("Exception in autocomplete",e);
+		}
+		
+		return suggestions;
 	}
 
 }
